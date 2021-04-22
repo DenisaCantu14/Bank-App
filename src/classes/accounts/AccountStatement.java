e608478926b88740d8a6a02ffcd2b6467de0df8e
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-22 11:57:56 +0300

    creare constructori

--- src/classes/accounts/AccountStatement.java
+++ src/classes/accounts/AccountStatement.java
@@ -1,48 +1,106 @@
 package classes.accounts;
 
-import classes.Client;
-import classes.Transaction;
+import classes.transactions.TransactionService;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class AccountStatement {
-    private Account account;
-    private Client client;
-    private ArrayList<Transaction> transactions;
+    private static Integer nrStatements = 0;
+    private Integer ID;
+    private double balance;
+    private Calendar startDate;
+    private Calendar createDate;
+    private Integer idAccount;
+    private ArrayList<Integer> idTransactions;
 
-    public Account getAccount() {
-        return account;
+    public AccountStatement(Integer idAccount, Calendar startDate) throws Exception {
+        this.ID =nrStatements++;
+        this.idAccount = idAccount;
+        this.balance = AccountService.getAccountById(idAccount).getBalance();
+        this.startDate = startDate;
+        this.createDate = Calendar.getInstance();
+        this.idTransactions =  TransactionService.getTransactionsByIdAccount(idAccount, startDate);
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
+    public AccountStatement(Integer ID, Integer idAccount, String startDate, String createDate, double balance, ArrayList<Integer> idTransactions) throws ParseException {
+        nrStatements++;
+        this.ID = ID;
+        this.idAccount = idAccount;
+        this.idTransactions = idTransactions;
+        this.balance = balance;
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+        Date date = sdf.parse(createDate);
+        this.createDate = sdf.getCalendar();
+
+        sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
+        date = sdf.parse(startDate);
+        this.startDate = sdf.getCalendar();
+
     }
 
-    public Client getClient() {
-        return client;
+    public Integer getID() {
+        return ID;
     }
 
-    public void setClient(Client client) {
-        this.client = client;
+    public void setID(Integer ID) {
+        this.ID = ID;
     }
 
-    public ArrayList<Transaction> getTransactions() {
-        return transactions;
+    public Integer getIdAccount() {
+        return idAccount;
     }
 
-    public void setTransactions(ArrayList<Transaction> transactions) {
-        this.transactions = transactions;
+    public void setIdAccount(Integer idAccount) {
+        this.idAccount = idAccount;
+    }
+
+    public ArrayList<Integer> getIdTransactions() {
+        return idTransactions;
+    }
+
+    public void setIdTransactions(ArrayList<Integer> idTransactions) {
+        this.idTransactions = idTransactions;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public Calendar getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Calendar createDate) {
+        this.createDate = createDate;
     }
 
     @Override
     public String toString() {
-        String afisare = "Account statement" + '\n' +
-                         "Account: " + account.toString() + '\n' +
-                         "Client: " + client.toString() + '\n';
+        String afisare =
+                        "Account statement" + '\n' +
+                        "ID: " + ID + '\n' +
+                        "IdAccount: " + idAccount + '\n' +
+                        "Current balance: " + balance + '\n' +
+                        "Start Date: " + startDate.getTime() + '\n' +
+                        "Create Date: " + createDate + '\n';
 
-        for (int i = 0; i <= transactions.size(); i++) {
-            afisare += transactions.get(i).toString();
+        for (int i : idTransactions) {
+            afisare += TransactionService.getTransactionById(i).toString();
         }
         return afisare;
     }
@@ -52,11 +110,11 @@ public class AccountStatement {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         AccountStatement that = (AccountStatement) o;
-        return Objects.equals(account, that.account) && Objects.equals(client, that.client) && Objects.equals(transactions, that.transactions);
+        return Double.compare(that.balance, balance) == 0 && Objects.equals(ID, that.ID) && Objects.equals(startDate, that.startDate) && Objects.equals(createDate, that.createDate) && Objects.equals(idAccount, that.idAccount) && Objects.equals(idTransactions, that.idTransactions);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(account, client, transactions);
+        return Objects.hash(ID, balance, startDate, createDate, idAccount, idTransactions);
     }
 }
