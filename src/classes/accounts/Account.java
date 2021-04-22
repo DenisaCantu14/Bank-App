3a23e5450768603a16eed7dc56e1c34ef75c0f9b
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-22 11:55:31 +0300

    adaugare idclient & idstatement

--- src/classes/accounts/Account.java
+++ src/classes/accounts/Account.java
@@ -11,16 +11,19 @@ abstract public class Account {
     protected String IBAN;
     protected double balance;
     protected Calendar createDate;
+    protected Integer IdClient;
+    protected Integer statementId;
 
-    public Account(double balance)
+    public Account(double balance, Integer idClient)
     {
         this.ID = nrAccounts++;
         this.IBAN = "R012" + ID;
         this.balance = balance;
         this.createDate = Calendar.getInstance();
+        this.IdClient = idClient;
     }
 
-    public Account(int ID, String IBAN, double balance, String  createDate) throws ParseException {
+    public Account(int ID, String IBAN, double balance, String  createDate, Integer idClient, Integer statementId) throws ParseException {
         this.ID = ID;
         nrAccounts ++;
         this.IBAN = IBAN;
@@ -29,6 +32,8 @@ abstract public class Account {
         SimpleDateFormat sdf = new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy", Locale.ENGLISH);
         Date date = sdf.parse(createDate);
         this.createDate = sdf.getCalendar();
+        this.IdClient = idClient;
+        this.statementId = statementId;
     }
 
     public int getID() {
@@ -63,13 +68,31 @@ abstract public class Account {
         this.createDate = createDate;
     }
 
+    public Integer getIdClient() {
+        return IdClient;
+    }
+
+    public void setIdClient(Integer idClient) {
+        IdClient = idClient;
+    }
+
+    public Integer getStatementId() {
+        return statementId;
+    }
+
+    public void setStatementId(Integer statementId) {
+        this.statementId = statementId;
+    }
+
     @Override
     public String toString() {
         return "Account" + '\n' +
                 "ID" + ID + '\n' +
                 "IBAN: " + IBAN + '\n' +
                 "Balance: " + balance + '\n' +
-                "Create Date: " + createDate.getTime() + '\n';
+                "Create Date: " + createDate.getTime() +
+                "Client Id: " + IdClient +
+                "Statement ID: " + statementId + '\n';
     }
 
     @Override
@@ -77,12 +100,12 @@ abstract public class Account {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Account account = (Account) o;
-        return ID == account.ID && Double.compare(account.balance, balance) == 0 && Objects.equals(IBAN, account.IBAN) && Objects.equals(createDate, account.createDate);
+        return ID == account.ID && Double.compare(account.balance, balance) == 0 && Objects.equals(IBAN, account.IBAN) && Objects.equals(createDate, account.createDate) && Objects.equals(IdClient, account.IdClient) && Objects.equals(statementId, account.statementId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(ID, IBAN, balance, createDate);
+        return Objects.hash(ID, IBAN, balance, createDate, IdClient, statementId);
     }
 
     abstract public void withdraw(double amount);
